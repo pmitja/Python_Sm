f0066a5284e63aed0dcf2808ff9ea9b03e82d0f0
pmitja <62394071+pmitja@users.noreply.github.com>
2020-10-29 18:40:54 +0100

    Delete guess_secret_number.py

--- guess_secret_number.py
+++
@@ -1,8 +0,0 @@
-secret = 3
-
-user_number = int(input("Please pick one number between 1 and 30: "))
-
-if secret == user_number:
-    print(f"You guess right number! The number is {user_number}")
-else:
-    print(f"Sorry you picked wrong number. Right number was {secret}")
